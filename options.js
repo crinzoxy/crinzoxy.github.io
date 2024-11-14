ab7a81166a06241d9b349070b33d28121c627fee
Crinzoxy <notmixie64@gmail.com>
2024-11-14 09:08:23 +0100

    options.js

--- options.js
+++ options.js
@@ -12,3 +12,14 @@ document.addEventListener('selectionchange', () => {
         }, 4000); // 4000 milliseconds = 4 seconds
     }
 });
+  async src="https://www.googletagmanager.com/gtag/js?id=G-7PR5X6MPV7"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-7PR5X6MPV7');
+    
+
+         async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7953644567210200"
+     crossorigin="anonymous"
